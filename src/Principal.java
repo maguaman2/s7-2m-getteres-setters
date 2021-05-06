bf252ae489436848a6f57fe56d58769e23dc7675
Marco Guaman B <maguaman2@sudamericano.edu.ec>
2021-05-06 10:28:57 -0500

    tarea s7

---
+++ src/Principal.java
@@ -0,0 +1,6 @@
+public class Principal {
+    public static void main(String[] args) {
+        //Crear una clase con sus getters setters y constructores
+        // desde el main crear un objeto emeplo
+    }
+}
